b5263103f3c7fdedcb11df1cfd3e109d77ce0b26
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-04 14:33:42 -0500

    The project has started.

---
+++ SwiftDrill/SwiftDrill/SwiftDrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDrillApp.swift
+//  SwiftDrill
+//
+//  Created by Morshed Alam on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
