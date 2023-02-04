1498c9b3bb5ea8813e2294e4cee78b05c55a6637
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-04 17:37:17 -0500

    Added Game model

--- SwiftDrill/SwiftDrill/Views/ContentView.swift
+++ SwiftDrill/SwiftDrill/Views/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var alertISVisible: Bool = false
+    @State private var sliderValue : Double = 50.0
+    @State private var game: Game = Game()
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
@@ -17,7 +20,7 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .kerning(-1.0)
                 .font(.largeTitle)
                 .fontWeight(.black)
@@ -25,7 +28,7 @@ struct ContentView: View {
             HStack{
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
                 
@@ -37,7 +40,8 @@ struct ContentView: View {
                 Text("Hit me")
             }
             .alert(isPresented: $alertISVisible) {
-                return Alert(title: Text("Hello there!!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                var roundedValue = Int (self.sliderValue.rounded())
+                return Alert(title: Text("Hello there!!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue))"), dismissButton: .default(Text("Awesome!")))
             }
         }
     }
