e8ab1bb77831bd9cb08b008f7e86c04e16dbc562
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-04 17:19:39 -0500

    Slider, Button with action and Text and alert  has added.

--- SwiftDrill/SwiftDrill/ContentView.swift
+++ SwiftDrill/SwiftDrill/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertISVisible: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, SwiftUI!")
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            HStack{
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+                
+            }
+            Button(action: {
+                print("Hey SwiftUI!!")
+                self.alertISVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertISVisible) {
+                return Alert(title: Text("Hello there!!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
