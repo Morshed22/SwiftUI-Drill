2ac480f91c21c449d7bfc4cc542c5c69f9b9747f
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-05 14:15:51 -0500

    Game Model functionality and test case added

---
+++ SwiftDrill/SwiftDrill/Views/Shapes.swift
@@ -0,0 +1,20 @@
+//
+//  Shapes.swift
+//  SwiftDrill
+//
+//  Created by Morshed Alam on 5/2/23.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Shapes_Previews: PreviewProvider {
+    static var previews: some View {
+        Shapes()
+    }
+}
--- SwiftDrill/SwiftDrillTests/SwiftDrillTests.swift
+++ SwiftDrill/SwiftDrillTests/SwiftDrillTests.swift
@@ -10,25 +10,63 @@ import XCTest
 
 final class SwiftDrillTests: XCTestCase {
 
-    var game : Game!
-    
+    var game: Game!
+
     override func setUpWithError() throws {
-        game = Game()
+      game = Game()
     }
 
     override func tearDownWithError() throws {
-       game = nil
+      game = nil
     }
 
     func testScorePositive() {
-        var guess = game.target + 5
-        var score = game.points(sliderValue: guess)
-        XCTAssertEqual(score, 95)
+      let guess = game.target + 5
+      let score = game.points(sliderValue: guess)
+      XCTAssertEqual(score, 95)
     }
-    
+
     func testScoreNegative() {
-        var guess = game.target - 5
-        var score = game.points(sliderValue: guess)
-        XCTAssertEqual(score, 95)
+      let guess = game.target - 5
+      let score = game.points(sliderValue: guess)
+      XCTAssertEqual(score, 95)
     }
-}
+
+    func testNewRound() {
+      game.startNewRound(points: 100)
+      XCTAssertEqual(game.score, 100)
+      XCTAssertEqual(game.round, 2)
+    }
+
+    func testScoreExact() {
+      let guess = game.target
+      let score = game.points(sliderValue: guess)
+      XCTAssertEqual(score, 200)
+    }
+
+    func testScoreClose() {
+      let guess = game.target + 2
+      let score = game.points(sliderValue: guess)
+      XCTAssertEqual(score, 98 + 50)
+    }
+
+    func testRestart() {
+      game.startNewRound(points: 100)
+      XCTAssertNotEqual(game.score, 0)
+      XCTAssertNotEqual(game.round, 1)
+      game.restart()
+      XCTAssertEqual(game.score, 0)
+      XCTAssertEqual(game.round, 1)
+    }
+
+    func testLeaderboard() {
+      game.startNewRound(points: 100)
+      XCTAssertEqual(game.leaderboardEntries.count, 1)
+      XCTAssertEqual(game.leaderboardEntries[0].score, 100)
+      game.startNewRound(points: 200)
+      XCTAssertEqual(game.leaderboardEntries.count, 2)
+      XCTAssertEqual(game.leaderboardEntries[0].score, 200)
+      XCTAssertEqual(game.leaderboardEntries[1].score, 100)
+    }
+
+  }
