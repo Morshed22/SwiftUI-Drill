21b879442c4fcf8f89577b3bb0943912c9aec0c6
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-05 14:40:41 -0500

    PonitsView and ShapView has added

--- SwiftDrill/SwiftDrill/Views/PointsView.swift
+++ SwiftDrill/SwiftDrill/Views/PointsView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct PointsView: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let roundedValue = Int(sliderValue.rounded())
+        let points = game.points(sliderValue: roundedValue)
+        
+        VStack(spacing: 10) {
+            InstructionText(text: "The slider's value is")
+            BigNumberText(text: String(roundedValue))
+            BodyText(text: "You scored \(points) Points\n🎉🎉🎉")
+            Button {
+                withAnimation {
+                    alertIsVisible = false
+                }
+                game.startNewRound(points: points)
+            } label: {
+                ButtonText(text: "Start New Round")
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300)
+        .background(Color("BackgroundColor"))
+        .cornerRadius(Constants.General.roundRectCornerRadius)
+        .shadow(radius: 10, x: 5, y: 5)
     }
 }
 
 struct PointsView_Previews: PreviewProvider {
+    
+    static private var alertIsVisible = Binding.constant(false)
+    static private var sliderValue = Binding.constant(50.0)
+    static private var game = Binding.constant(Game())
+
     static var previews: some View {
-        PointsView()
+        PointsView(alertIsVisible: alertIsVisible, sliderValue: sliderValue, game: game)
+        PointsView(alertIsVisible: alertIsVisible, sliderValue: sliderValue, game: game)
+          .preferredColorScheme(.dark)
     }
 }
--- SwiftDrill/SwiftDrill/Views/Shapes.swift
+++ SwiftDrill/SwiftDrill/Views/Shapes.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct Shapes: View {
+    @State private var wideShapes =  true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if !wideShapes{
+                Circle()
+                    .strokeBorder(Color.blue, lineWidth: 20.0)
+                    .frame(width: 200.0, height: 100.0)
+                    .transition(.opacity)
+            }
+            RoundedRectangle(cornerRadius: 20.0)
+                .fill(Color.blue)
+                .frame(width: wideShapes ? 200 : 100, height: 100.0)
+            
+            Capsule()
+                .fill(Color.blue)
+                .frame(width: wideShapes ? 200 : 100, height: 100.0)
+            
+            Ellipse()
+              .fill(Color.blue)
+              .frame(width: wideShapes ? 200 : 100, height: 100.0)
+            
+            Button(action: {
+              withAnimation {
+                wideShapes.toggle()
+              }
+            }) {
+              Text("Animate!")
+            }
+        }
+        .background(Color.gray)
     }
 }
 
