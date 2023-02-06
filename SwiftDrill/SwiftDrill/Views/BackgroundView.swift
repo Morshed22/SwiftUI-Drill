12892d228eb991bec4889ed5cdba29f4fe9cd981
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-06 00:49:01 -0500

    BackGroundView updated

--- SwiftDrill/SwiftDrill/Views/BackgroundView.swift
+++ SwiftDrill/SwiftDrill/Views/BackgroundView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct BackgroundView: View {
     @Binding var game: Game
+    
     var body: some View {
         VStack {
             TopView(game: $game)
-          //Spacer()
-         // BottomView(game: $game)
+            Spacer()
+            BottmView(game: $game)
         }
+        .padding()
+        .background(
+            RingsView()
+        )
+        
     }
 }
 
@@ -25,9 +31,9 @@ struct TopView: View{
     var body: some View{
         HStack{
             Button(action: {
-              game.restart()
+                game.restart()
             }) {
-              RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+                RoundedImageViewStroked(systemName: "arrow.counterclockwise")
             }
             Spacer()
             Button {
@@ -35,9 +41,52 @@ struct TopView: View{
             } label: {
                 RoundedImageViewFilled(systemName: "list.dash")
             }.sheet(isPresented: $leaderboardIsShowing) {
-               // Leaderbo
+                LeaderboardView(leaderboardIsShowing: $leaderboardIsShowing, game: $game)
             }
+            
+        }
+    }
+}
 
+struct NumberView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            LabelText(text: title.uppercased())
+            RoundRectTextView(text: text)
+        }
+    }
+}
+
+struct BottmView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            NumberView(title: "Score", text: String(game.score))
+            Spacer()
+            NumberView(title: "Round", text: String(game.round))
+        }
+    }
+}
+
+struct RingsView: View {
+    
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            ForEach(1..<6) { ring in
+                let size = CGFloat(ring * 100)
+                Circle()
+                    .stroke(lineWidth: 20.0)
+                    .fill(
+                        RadialGradient(gradient: Gradient(colors: [Color("RingsColor").opacity(0.3 * 0.8), Color("RingsColor").opacity(0)]), center: .center, startRadius: 100, endRadius: 300)
+                    )
+                    .frame(width: size, height: size)
+            }
         }
     }
 }
